e14c2a6b3b7731b15ca1e6cb34c49c9bdbfb4528
aabaroab <72941918+aabaroab@users.noreply.github.com>
2021-04-29 16:09:59 +0200

    Update testDividir.java

--- testDividir.java
+++ testDividir.java
@@ -1,7 +1,10 @@
 class testDividir{
     public static void main(String args[]){
+        Random r = new Random();
+        int low = 0;
+        int high = 1;
+        int randomNumber = r.nextInt(high-low) + low;
         int initialNumber = 10;
-        int randomNumber =Math.round(Math.random());
-        system.out.println(initialNumber/randomNumber);
+        System.out.println(initialNumber/randomNumber);
     }
 }
