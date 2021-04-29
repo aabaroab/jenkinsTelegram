837c2913d1ed36d3683cd41ea9de9919993cb165
aabaroab <72941918+aabaroab@users.noreply.github.com>
2021-04-29 15:54:56 +0200

    Add files via upload

---
+++ testDividir.java
@@ -0,0 +1,7 @@
+class testDividir{
+    public static void main(String args[]){
+        int initialNumber = 10;
+        int randomNumber =Math.round(Math.random());
+        system.out.println(initialNumber/randomNumber);
+    }
+}
