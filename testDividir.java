414f449118f16fd3568b0fba625f69e90d45bc1c
aabaroab <72941918+aabaroab@users.noreply.github.com>
2021-04-30 19:54:56 +0200

    Update testDividir.java

--- testDividir.java
+++ testDividir.java
@@ -1,14 +1,7 @@
 import java.util.Random;
 class testDividir{
-    public static void main(String args[]){
-        /*
-        Random r = new Random();
-        int low = 0;
-        int high = 1;
-        int randomNumber = r.nextInt(high-low) + low;
-        int initialNumber = 10;
-        System.out.println(initialNumber);
-        */
-        System.out.println("Hello");
+    public static void main(String[] args) {
+        int i=0;
+        System.out.println("Seed " + i + ": " + RandomNumberGenerator(i, 0, 1));
     }
 }
